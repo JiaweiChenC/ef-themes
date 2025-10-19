1f578cb238e2e1857e95f3049c7ea4efd2199c95
JiaweiChenC <jc5667@columbia.edu>
2025-10-18 20:40:56 -0400

    comment color lighter

--- ef-filter-sun-theme.el
+++ ef-filter-sun-theme.el
@@ -50,7 +50,7 @@
       (bg-main     "#f8efe7")
       (fg-main     "#2c232e")
       (bg-dim      "#ecddd0")
-      (fg-dim      "#72696d")
+      (fg-dim      "#a09a9c")
       (bg-alt      "#fdf7f3")
       (fg-alt      "#92898a")
 
